eb24e23ecfa18c3f92f9a8b220bddccbf89b1ab4
Andrey <v.andrei.dmitrievich@gmail.com>
2024-04-24 16:16:27 +0300

    fix transaction

--- pereval/pereval_app/views.py
+++ pereval/pereval_app/views.py
@@ -9,7 +9,7 @@ from .models import PerUser, Cords, Level, Pereval
 
 
 class SubmitData(views.APIView):
-    @transaction.atomic
+
     def post(self, request, *args, **kwargs):
 
         try:
@@ -22,30 +22,31 @@ class SubmitData(views.APIView):
                                       'message': f'Ошибка в названии поля {e}'})
 
         try:
-            user_serializer = PerUserSerializer(data=user)
-            user_serializer.is_valid(raise_exception=True)
-            user_serializer.save()
+            with transaction.atomic():
+                user_serializer = PerUserSerializer(data=user)
+                user_serializer.is_valid(raise_exception=True)
+                user_serializer.save()
 
-            cords_serializer = CordsSerializer(data=cords)
-            cords_serializer.is_valid(raise_exception=True)
-            cords_serializer.save()
+                cords_serializer = CordsSerializer(data=cords)
+                cords_serializer.is_valid(raise_exception=True)
+                cords_serializer.save()
 
-            level_serializer = LevelSerializer(data=level)
-            level_serializer.is_valid(raise_exception=True)
-            level_serializer.save()
+                level_serializer = LevelSerializer(data=level)
+                level_serializer.is_valid(raise_exception=True)
+                level_serializer.save()
 
-            request.data['user'] = PerUser.objects.last().pk
-            request.data['coords'] = Cords.objects.last().pk
-            request.data['level'] = Level.objects.last().pk
-            pereval_serializer = PerevalSerializer(data=request.data)
-            pereval_serializer.is_valid(raise_exception=True)
-            pereval_serializer.save()
+                request.data['user'] = PerUser.objects.last().pk
+                request.data['coords'] = Cords.objects.last().pk
+                request.data['level'] = Level.objects.last().pk
+                pereval_serializer = PerevalSerializer(data=request.data)
+                pereval_serializer.is_valid(raise_exception=True)
+                pereval_serializer.save()
 
-            for image in images:
-                image['pereval'] = Pereval.objects.last().pk
-                image_serializer = ImageSerializer(data=image)
-                image_serializer.is_valid()
-                image_serializer.save()
+                for image in images:
+                    image['pereval'] = Pereval.objects.last().pk
+                    image_serializer = ImageSerializer(data=image)
+                    image_serializer.is_valid()
+                    image_serializer.save()
         except Exception as e:
             return response.Response({'status': status.HTTP_500_INTERNAL_SERVER_ERROR,
                                       'message': f'Данные не сохранены. Ошибка: {e}'})
