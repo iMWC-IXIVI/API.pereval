70cde07c1febbc7181c77bce95041b6149671bad
Andrey <v.andrei.dmitrievich@gmail.com>
2024-04-27 15:53:20 +0300

    fix get informations

--- pereval/pereval_app/views.py
+++ pereval/pereval_app/views.py
@@ -62,25 +62,19 @@ class SubmitData(views.APIView):
 class DetailSubmitData(views.APIView):
     def get(self, request, *args, **kwargs):
         pk = kwargs.get('pk')
-        email = kwargs.get('email')
 
-        if request.path == f'/submitData/{pk}/':
+        pereval = Pereval.objects.get(pk=pk)
 
-            pereval = Pereval.objects.get(pk=pk)
+        serializer = PerevalSerializer(pereval).data
+        serializer['user'] = PerUserSerializer(PerUser.objects.get(pk=serializer['user'])).data
+        serializer['coords'] = CordsSerializer(Cords.objects.get(pk=serializer['coords'])).data
+        serializer['level'] = LevelSerializer(Level.objects.get(pk=serializer['level'])).data
+        serializer['images'] = ImageSerializer(Image.objects.filter(pereval_id=pk), many=True).data
 
-            serializer = PerevalSerializer(pereval).data
-            serializer['user'] = PerUserSerializer(PerUser.objects.get(pk=serializer['user'])).data
-            serializer['coords'] = CordsSerializer(Cords.objects.get(pk=serializer['coords'])).data
-            serializer['level'] = LevelSerializer(Level.objects.get(pk=serializer['level'])).data
-            serializer['images'] = ImageSerializer(Image.objects.filter(pereval_id=pk), many=True).data
+        for data in serializer['images']:
+            del data['pereval']
 
-            for data in serializer['images']:
-                del data['pereval']
-
-            return response.Response({'data': serializer})
-
-        elif request.path == f'/submitData/&user__email={email}':
-            ...
+        return response.Response({'data': serializer})
 
     def put(self, request, *args, **kwargs):
         pass
